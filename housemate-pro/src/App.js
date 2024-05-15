b51c3acbdad9669201cb795defdf9086470275d8
Giselle Arboleda <garboled@uci.edu>
2024-05-15 10:11:06 -0700

    Blank React project

---
+++ housemate-pro/src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div>
+      <header >
+        Yay! Hi, Niko and Thomas! Let's get started on the project :)
+      </header>
+    </div>
+  );
+}
+
+export default App;
