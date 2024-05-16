557cc3257a557e18c46a8b65aa52e91450528d1b
Giselle Arboleda <garboled@uci.edu>
2024-05-15 17:59:51 -0700

    Added user buttons

--- housemate-pro/src/App.tsx
+++ housemate-pro/src/App.tsx
@@ -1,7 +1,11 @@
+import Button from "./components/Button";
+
 function App() {
   return (
     <div>
       <h1>Hello, Niko and Thomas! Let's get started on this project :)</h1>
+      <Button></Button>
+      <Button></Button>
     </div>
   );
 }
---
+++ housemate-pro/src/components/Button.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import "./Button.css";
+const Button = () => {
+  return (
+    <button type="button" className="btn btn-primary btn-circle">
+      User1
+    </button>
+  );
+};
+
+export default Button;
