2000d11c40243d3856d09356d416b712ce2f90ff
treynol <83850719+treynol@users.noreply.github.com>
2024-05-20 22:14:05 +0000

    nav bar remove unneeded options

--- housemate-pro/src/components/NavBar.tsx
+++ housemate-pro/src/components/NavBar.tsx
@@ -27,21 +27,6 @@ function NavBar() {
         </button>
         <div className="collapse navbar-collapse" id="navbarNavDropdown">
           <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Features
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Pricing
-              </a>
-            </li>
             <li className="nav-item dropdown">
               <a
                 className="nav-link dropdown-toggle"
